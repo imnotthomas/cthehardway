b00fa9fafbd01c1ce81899a50a6b2b117b113c8c
Thomas Altman <thomasjaltman@gmail.com>
2013-12-27 02:14:22 +0000

    ex7

--- Makefile
+++ Makefile
@@ -4,4 +4,4 @@ clean:
 	rm -f ex1 ex1.c~ ex3 ex3.c~ Makefile~ ex4 ex4.c~
 
 all:
-	make ex1 ex3 ex4
+	make ex1 ex3 ex4 ex7
--- ex7.c
+++ ex7.c
@@ -10,5 +10,16 @@ int main(int argc, char *argv[])
   long universe_of_defects = 1L * 1024L * 1024L * 1024L;
   printf("The entire univese has %ld bugs.\n", universe_of_defects);
 
+  double expected_bugs = bugs * bug_rate;
+  printf("You are expected to have %f bugs.\n",
+	 expected_bugs);
+
+  double part_of_universe = expected_bugs / universe_of_defects;
+  printf("That is only %e portion of the universe.\n", part_of_universe);
+
+  char nul_byte = '\0';
+  int care_percentage = bugs * nul_byte;
+  printf("Which means you should care %d%%.\n", care_percentage);
+
   return 0;
 }
