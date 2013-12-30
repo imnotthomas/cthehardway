0ccff31bdef11b87d283fb6e0d2c2c4f776874b1
Thomas Altman <thomasjaltman@gmail.com>
2013-12-30 01:40:39 +0000

    ex9

--- ex8.c
+++ ex8.c
@@ -9,13 +9,17 @@ int main(int argc, char *argv[])
     ' ', 'J', '.',
     ' ', 'A', 'l', 't', 'm', 'a', 'n', '\0'
   };
+  areas[0] = 100;
+  areas[1] = name[0];
   
   printf("The size of an int: %ld\n", sizeof(int));
   printf("The size of areas (int[]): %ld\n",
 	 sizeof(areas));
   printf("The number of ints in areas: %ld\n",
 	 sizeof(areas) / sizeof(int));
-
+  printf("areas[0]: %d\nareas[1]: %d\n",
+	 areas[0], areas[1]);
+  
   printf("The size of a char: %ld\n",
 	 sizeof(char));
   printf("The size of name (char[]): %ld\n",
