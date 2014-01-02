032c8c93fee24badfed91e2ad7f0238fb9052b6c
Thomas Altman <thomasjaltman@gmail.com>
2014-01-02 01:17:11 +0000

    ex14

--- ex14.c
+++ ex14.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 // forward declarations
 int can_print_it(char ch);
-void print_letters(char arg[]);
+void print_letters(int str_len, char arg[]);
 
 void print_args(int argc, char *argv[])
 {
   int i =0;
-
+  int str_len = 0;
+  
   for(i = 0; i < argc; i++) {
-    print_letters(argv[i]);
+    str_len = strlen(argv[i]);
+    print_letters(str_len, argv[i]);
   }
 }
 
-void print_letters(char arg[])
+void print_letters(int str_len, char arg[])
 {
   int i = 0;
 
-  for(i = 0; arg[i] != '\0'; i++) {
+  for(i = 0; i < str_len; i++) {
     char ch = arg[i];
 
     if(can_print_it(ch)) {
