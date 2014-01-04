3c463a9087fbcf72d312188b71e0b2bb500f48dc
Thomas Altman <thomasjaltman@gmail.com>
2014-01-04 03:08:49 +0000

    ex17

---
+++ ex17.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#define MAX_DATA 512
+#define MAX_ROWS 100
+
+struct Address {
+  // makes an address structure
+  int id;
+  int set;
+  char name[MAX_DATA];
+  char email[MAX_DATA];
+};
+
+struct Database {
+  // a structure of Address structures
+  struct Address rows[MAX_ROWS];
+};
+
+struct Connection {
+  FILE *file;
+  struct Database *db;
+};
+
+void die(const char *message)
+{
+  if(errno) {
+    perror(message);
+  }
+  else {
+    printf("ERROR: %s\n", message);
+  }
+
+  exit(1);
+}
+
+void Address_print(struct Address *addr)
+{
+  printf("%d %s %s", addr->id, addr->name, addr->email);
+}
+
+void Database_load(struct Connection *conn)
+{
+  int rc = fread(conn->db, sizeof(struct Database), 1, conn->file);
+  if(rc != 1) {
+    die("Failed to load database.");
+  }
+}
+
+struct Connection *Database_open(const char *filename, char mode)
+{
+  struct Connection *conn = malloc(sizeof(struct Connection));
+  if(!conn) {
+    die("Memory error");
+  }
+
+  conn->db = malloc(sizeof(struct Database));
+  if(!conn->db) die("Memory error");
+
+  if(mode == 'c') {
+    conn->file = fopen(filename, "w");
+  }
+  else {
+    conn->file = fopen(filename, "r+");
+
+    if(conn->file) {
+      Database_load(conn);
+    }
+  }
+
+  if(!conn->file) die("Failed to open the file");
+
+  return(conn);
+}
+
+void Database_close(struct Connection *conn)
+{
+  if(conn) {
+    if(conn->file) fclose(conn->file);
+    if(conn->db) free(conn->db);
+    free(conn);
+  }
+}
+
+void Database_write(struct Connection *conn)
+{
+  rewind(conn->file);
+
+  int rc = fwrite(conn->db, sizeof(struct Database), 1, conn->file);
+  if(rc != 1) die("Failed to write to database");
+
+  rc = fflush(conn->file);
+  if(rc == -1) die("Cannot flush database.");
+}
+
+int main(int argc, char *argv[])
+{
+  return 0;
+}
